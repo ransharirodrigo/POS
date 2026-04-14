39714f56b14f152f33fc714f5b649f37dd37415d
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2026-04-15 02:35:37 +0530

    Permission check in the sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -6,8 +6,14 @@
         <a href="{{ route('dashboard') }}" class="{{ request()->routeIs('dashboard') ? 'active' : '' }}"><i class="bi bi-grid-1x2"></i> @lang('messages.nav.dashboard')</a>
         @if(Auth::user()->role === 'super admin')
             <a href="{{ route('staff.index') }}" class="{{ request()->routeIs('staff.*') ? 'active' : '' }}"><i class="bi bi-people"></i> @lang('messages.nav.staff')</a>
-        @endif
-        <a href="#"><i class="bi bi-box-seam"></i> @lang('messages.nav.products')</a>
+        @endif 
+        @canany(['category view', 'category manage'])
+            <a href="{{ route('categories.index') }}" class="{{ request()->routeIs('categories.*') ? 'active' : '' }}"><i class="bi bi-tags"></i> @lang('messages.categories.title')</a>
+        @endcanany
+        @canany(['product view', 'product manage'])
+            <a href="{{ route('products.index') }}" class="{{ request()->routeIs('products.*') ? 'active' : '' }}"><i class="bi bi-box-seam"></i> @lang('messages.nav.products')</a>
+        @endcanany
+       
         <a href="#"><i class="bi bi-cart3"></i> @lang('messages.nav.sales')</a>
         <a href="#"><i class="bi bi-graph-up"></i> @lang('messages.nav.reports')</a>
         <a href="#"><i class="bi bi-gear"></i> @lang('messages.nav.settings')</a>
