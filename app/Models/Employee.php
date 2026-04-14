70edaaab8538b824421e06a6ec8b8988419b73ad
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2026-04-15 02:36:38 +0530

    Employee model change

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class Employee extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
 
     protected $fillable = [
         'first_name',
