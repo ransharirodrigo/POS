edb41fb0c5e8f3381b1721053758b1d2f4ab22aa
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2026-04-15 13:47:43 +0530

    Add permission middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'super.admin' => \App\Http\Middleware\EnsureUserIsSuperAdmin::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
